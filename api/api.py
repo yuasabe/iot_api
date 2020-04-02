1f194c5dd64008c2c2ecbe670edf5623be78c011
Yu Asabe <yuasabe@g.ecc.u-tokyo.ac.jp>
2020-04-03 07:15:29 +0900

    Update for dht11

--- api/api.py
+++ api/api.py
@@ -4,34 +4,27 @@ from flask import request, jsonify
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-# Create some test data for our catalog in the form of a list of dictionaries.
-books = [
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
-    {'id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-     'published': '1973'},
-    {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'published': '1975'}
-]
-
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return "<h1>IOT API</h1><p>This site is a prototype API for IOT sensing, namely the DHT11 for now.</p>"
 
 # A route to return all of the available entries in our catalog.
-@app.route('/api/v1/resources/books/all', methods=['GET'])
-def api_all():
-    return jsonify(books)
+@app.route('/api/v1/resources/dht11', methods=['POST'])
+def api_add():
+	req_data = request.get_json()
+	temp = req_data['temp']
+	humidity = req_data['humidity']
+	print(temp, humidity)
+	response = {"status":"success"}
+	return response, 200
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404</h1><p>The resource could not be found.</p>", 404
+
+
 app.run()
 
 
